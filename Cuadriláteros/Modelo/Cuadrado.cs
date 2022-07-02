fd2eca4b375dec75c4fdf5c1968d2213428e7899
Gerardo Fabián Quispe <gfq0684@gmail.com>
2022-07-02 09:45:38 -0300

    Agregamos las clases Rectangulo y cuadrilatero

---
+++ Cuadriláteros/Modelo/Cuadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadriláteros.Modelo
+{
+    internal class Cuadrado : Cuadrilatero
+    {
+        public Cuadrado() : base()
+        {
+
+        }
+
+        public override double CalcularArea()
+        {
+            double area = 0;
+
+            return area;
+        }
+    }
+}
