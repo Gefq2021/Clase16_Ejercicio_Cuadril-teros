8450e57d3ad454c52860731e166383bb4dc3988a
Gerardo Fabián Quispe <gfq0684@gmail.com>
2022-07-01 23:33:09 -0300

    Agregamos la clase Trapecio y agremaos su constructor y el metodo CalcularArea

---
+++ Cuadriláteros/Modelo/Trapecio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadriláteros.Modelo
+{
+    public class Trapecio : Cuadrilatero
+    {
+        public Trapecio() : base()
+        {
+        }
+
+        public override double CalcularArea()
+        {
+            double area = 0;
+
+            return area;
+        }
+    }
+}
