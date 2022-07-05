f0b1049291ee74015ae2df17208357a180ff7f0f
Gerardo Fabián Quispe <gfq0684@gmail.com>
2022-07-04 23:28:50 -0300

    Agregamos ejemplos de prueba

--- Cuadriláteros/Program.cs
+++ Cuadriláteros/Program.cs
@@ -23,3 +23,38 @@
 
     Investigue las fórmulas para calcular el área de cada figura.
  */
+
+using Cuadriláteros.Modelo;
+
+// Trapecio
+Trapecio trapecio1 = new Trapecio();
+trapecio1.Vertice_1[0] = 0;
+trapecio1.Vertice_1[1] = 0;
+trapecio1.Vertice_2[0] = 1;
+trapecio1.Vertice_2[1] = 3;
+trapecio1.Vertice_3[0] = 3;
+trapecio1.Vertice_3[1] = 3;
+trapecio1.Vertice_4[0] = 4;
+trapecio1.Vertice_4[1] = 0;
+
+Console.WriteLine($"El Area del Trapecio es {trapecio1.CalcularArea()}");
+Console.WriteLine();
+
+// Rectangulo
+Rectangulo rectangulo1 = new Rectangulo();
+rectangulo1.Vertice_1[0] = 0;
+rectangulo1.Vertice_1[1] = 0;
+rectangulo1.Vertice_2[0] = 0;
+rectangulo1.Vertice_2[1] = 3;
+rectangulo1.Vertice_3[0] = 4;
+rectangulo1.Vertice_3[1] = 3;
+Console.WriteLine($"El Area del Rectangulo es {rectangulo1.CalcularArea()}");
+Console.WriteLine();
+
+// Cuadrado
+Cuadrado cuadrado1 = new Cuadrado();
+cuadrado1.Vertice_1[0] = 0;
+cuadrado1.Vertice_1[1] = 0;
+cuadrado1.Vertice_2[0] = 0;
+cuadrado1.Vertice_2[1] = 3;
+Console.WriteLine($"El Area del Cuadrado es {cuadrado1.CalcularArea()}");
